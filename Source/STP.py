625d3a95351cdd89b6c73a2070e5e4f5274882c3
YourtionGuo <yourtion@gmail.com>
2016-12-23 11:02:58 +0800

    add source

---
+++ Source/STP.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+
+from biplist import *
+import json
+
+class SourceTree:
+
+  filePath="/Users/Yourtion/Library/Application Support/SourceTree/browser.plist"
+
+  def _getProjects(self):
+    try:
+      plist = readPlist(self.filePath)
+      tempName = ""
+      res = []
+      for item in plist['$objects']:
+        if(type(item) is str and item[:1] == '/'):
+          res.append([tempName, item])
+        elif (type(item) is str):
+          tempName = item
+      return res
+    except e:
+      return []
+
+  def getList(self):
+    items = []
+    projects = self._getProjects()
+    for p in projects:
+      item = {
+        'title': p[0],
+        'subtitle': p[1],
+        'arg': p[1]
+      }
+      items.append(item)
+    result = {'items': items}
+    print(json.dumps(result))
