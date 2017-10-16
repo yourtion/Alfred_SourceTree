f31bc1a52a9eef9e0cbfed7b1023f993d64bedef
YourtionGuo <yourtion@gmail.com>
2017-10-16 11:33:05 +0800

    Fixed #5 add substring matching

--- STP.py
+++ STP.py
@@ -2,11 +2,25 @@
 from biplist import *
 import json
 import os
+import re
 
 class SourceTree:
 
   homePath=os.path.expanduser('~')
   filePath= homePath + "/Library/Application Support/SourceTree/browser.plist"
+
+  def _camel_case_split(self, identifier):
+    matches = re.finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', identifier)
+    return [m.group(0) for m in matches]
+
+  def _splitMatchWords(self, title):
+    res = []
+    cam = self._camel_case_split(title)
+    for m in cam:
+      ret = re.split('-|_| |',m)
+      for n in ret:
+        res.append(n)
+    return ' '.join(res);
 
   def _getProjects(self):
     try:
@@ -29,7 +43,8 @@ class SourceTree:
       item = {
         'title': p[0],
         'subtitle': p[1],
-        'arg': p[1]
+        'arg': p[1],
+        'match': self._splitMatchWords(p[0])
       }
       items.append(item)
     result = {'items': items}
